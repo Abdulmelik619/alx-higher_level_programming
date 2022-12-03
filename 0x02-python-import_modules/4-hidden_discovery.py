f091ffee8d46ef17fb31e6b6f9c64c19567a0de5
Abdulmelik619 <abdulmelikambaw619@gmail.com>
2022-12-03 07:07:26 -0800

    f

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-if __name__=="__main__":
-    import hidden_4.pyc
-    a=sorted(dir(hidden_4.pyc))
-    for name in a:
-        if(name[:2]=="__"):
-            continue
-        print("{:s}".format(name))
+# 4-hidden_discovery.py
+# Brennan D Baraban <375@holbertonschool.com>
+
+if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
