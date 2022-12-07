02fccced99cdbd5722a5edf3d3b17106ebe324ae
Abdulmelik619 <abdulmelikambaw619@gmail.com>
2022-12-07 12:37:20 -0800

    f

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,7 +3,7 @@ def uniq_add(my_list=[]):
     sum=0
     my_list_2=[]
     for x in my_list:
-        if(x in my_list):
+        if(x in my_list_2):
             continue
         my_list_2.append(x)
     for t in my_list_2:
