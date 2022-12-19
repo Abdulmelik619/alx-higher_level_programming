6e6865b8e7c66fafc1cebc3ac0f232cf0c459e3e
Abdulmelik619 <abdulmelikambaw619@gmail.com>
2022-12-19 13:29:23 -0800

    g

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,8 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     i=0
-    for z in range(x)
+    z=0
+    for z in range(0,x)
         try:
             print("{:d}".format(my_list[z]),end='')
             i+=1
