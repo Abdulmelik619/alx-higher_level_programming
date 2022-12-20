49fa89e5925e565e8a98201427ed4b1992a08d7f
Abdulmelik619 <abdulmelikambaw619@gmail.com>
2022-12-20 12:28:03 -0800

    g

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+
+"""Define a class Square."""
+
 class Square:
+    """Define a class Square."""
+    
     def __init__(self,size):
+        """Define a class Square."""
         self.__size=size
