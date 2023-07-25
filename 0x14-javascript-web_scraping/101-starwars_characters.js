11111b924ffc0b6d7e0230d88f9d12a07957f843
Abdulmelik619 <abdulmelikambw619@gmail.com>
2023-07-25 21:39:19 +0200

    d

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -8,11 +8,14 @@ request(url, function (error, response, body) {
   }
 });
 
+
+
 function printCharacters (characters, index) {
   request(characters[index], function (error, response, body) {
     if (!error) {
       console.log(JSON.parse(body).name);
       if (index + 1 < characters.length) {
+        /*1*/
         printCharacters(characters, index + 1);
       }
     }
